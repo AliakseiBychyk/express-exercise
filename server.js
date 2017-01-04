32be72485e9468f34d7a3bfc11b4cf53cecaee08
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-01-04 17:38:08 +0300

    custom middleware

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ var express = require('express');
 
 var app = express();
 
+app.use(function (req, res, next) {
+  console.log('Hello from the middleware!');
+  next();
+})
+
 app.get('/', function (req, res) {
   res.send("Show me the way to the next whiskey bar, oh don't ask why...");
 });
-        // sample http://localhost:3002/user/Aleks?option=server-test
+
+// sample http://localhost:3002/user/Aleks?option=server-test
 app.get('/user/:user', function (req, res) {
   res.send('Page for user ' + req.params.user + ' with option ' + req.query.option);
 })

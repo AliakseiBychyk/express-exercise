98e6f72448602b1d0cb738ef2f6ac8d7dde62296
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-01-04 17:30:14 +0300

    some routing with options

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var app = express();
 app.get('/', function (req, res) {
   res.send("Show me the way to the next whiskey bar, oh don't ask why...");
 });
+        // sample http://localhost:3002/user/Aleks?option=server-test
+app.get('/user/:user', function (req, res) {
+  res.send('Page for user ' + req.params.user + ' with option ' + req.query.option);
+})
 
 var server = app.listen(3002, function () {
   console.log('Server running on http://localhost:' + server.address().port);
